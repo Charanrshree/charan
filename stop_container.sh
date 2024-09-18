503c27abc8641588115930a52e3a3c5c4418c933
charanshree <charanroyal.cr@gmail.com>
2024-09-18 19:03:00 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,42 +1,6 @@
 #!/bin/bash
+set -e
 
-# Check if the correct number of arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <container_name> <image_name>"
-    exit 1
-fi
-
-# Assign arguments to variables
-container_name=$1
-image_name=$2
-
-# Function to stop the container
-stop_container() {
-    container_id=$(docker ps -a -q --filter "name=$container_name")
-    if [ -n "$container_id" ]; then
-        echo "Stopping container $container_name..."
-        docker stop "$container_id"
-        echo "Container $container_name stopped."
-        echo "Removing container $container_name..."
-        docker rm "$container_id"
-        echo "Container $container_name removed."
-    else
-        echo "No container found with name: $container_name"
-    fi
-}
-
-# Function to remove the image
-remove_image() {
-    image_id=$(docker images -q "$image_name")
-    if [ -n "$image_id" ]; then
-        echo "Removing image $image_name..."
-        docker rmi "$image_id"
-        echo "Image $image_name removed."
-    else
-        echo "No image found with name: $image_name"
-    fi
-}
-
-# Stop the container and remove the image
-stop_container
-remove_image
+#stop the running container (if any)
+containerid='docker ps | awk -F ""'{print $1}''
+docker rm -f $containerid
